2e9274c2bf854ed847d3c9f557b7604e6dc3c541
Tsvetelin Pantev <tsvetelin.pantev@gmail.com>
2020-11-28 23:16:37 +0200

    only appear new order after payhere was clicked

--- src/components/payment-pending/payment-pending.component.jsx
+++ src/components/payment-pending/payment-pending.component.jsx
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useReducer } from "react";
 import CustomButton from "../custom-button/custom-button.component";
 
 import "./payment-pending.styles.scss";
 import "../form.styles.scss";
 
 const PaymentPending = ({ paymentUrl }) => {
+  const [, forceUpdate] = useReducer((x) => (x === 0 ? x + 1 : x - 1), 0);
+
   const newOrder = () => {
-    localStorage.removeItem("orderId");
+    localStorage.clear();
     window.location.reload();
+  };
+
+  const payHere = () => {
+    window.open(paymentUrl, "_blank");
+    localStorage.setItem("new-order-button-visible", true);
+    setTimeout(forceUpdate, 1000);
   };
 
   return (
     <div className="new-order">
       <p>Thank you for your order</p>
       <div className="buttons">
-        <CustomButton onClick={() => window.open(paymentUrl, "_blank")}>
-          Pay here
-        </CustomButton>
-        <CustomButton onClick={newOrder}>New Order</CustomButton>
+        <CustomButton onClick={payHere}>Pay here</CustomButton>
+        {localStorage.getItem("new-order-button-visible") ? (
+          <CustomButton onClick={newOrder}>New Order</CustomButton>
+        ) : null}
       </div>
     </div>
   );
