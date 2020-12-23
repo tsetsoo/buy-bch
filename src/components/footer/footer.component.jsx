ce74306783475489e73c2de68804f5f7928aa38c
Tsvetelin Pantev <tsvetelin.pantev@gmail.com>
2020-12-23 02:33:51 +0200

    https://gitlab.com/poype.io/buybch-frontend/-/issues/11 footer with company informaiton

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { useIntl } from "react-intl";
+
+import "./footer.styles.scss";
+
+const Footer = () => {
+  const intl = useIntl();
+
+  return (
+    <div className="footer">
+      <p>
+        {intl.formatMessage(
+          { id: "footer.footer" },
+          {
+            a: () => <a href={"https://www.poype.io/"}>{"POYPE LTD"}</a>,
+            number: "205728837",
+            email: "office@poype.io",
+            phone: "+359899837405",
+          }
+        )}
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
